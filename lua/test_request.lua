c45160011c53de0f24d46c40b5d3b38212e9dd92
loodeer <751564347@qq.com>
2019-04-09 17:23:20 +0000

    some lua usage

---
+++ lua/test_request.lua
@@ -0,0 +1,36 @@
+
+local var = ngx.var
+ngx.say("ngx.var.a:",var.a,"<br/>")
+ngx.say("ngx.var.b:",var.b,"<br/>")
+ngx.say("ngx.var[2]:",var[2],"<br/>")
+ngx.var.b = 2;
+ngx.say("<br/>")
+
+local headers = ngx.req.get_headers()
+ngx.say("headers begin","<br/>")
+ngx.say("Host:",headers["Host"],"<br/>")
+ngx.say("user-agent:",headers["user-agent"],"<br/>")
+ngx.say("user-agent:",headers.user_agent,"<br/>")
+
+for k,v in pairs(headers) do 
+    if type(v) == "table" then
+        ngx.say(k,":",table.concat(v,","),"<br/>")
+    else
+        ngx.say(k,":",v,"<br/>")
+    end
+end
+ngx.say("headers end","<br/>")
+ngx.say("<br/>")
+
+ngx.say("uri args begin","<br/>")
+local uri_args = ngx.req.get_uri_args()
+for k,v in pairs(uri_args) do
+    if type(v) == "table" then
+        ngx.say(k,":",table.concat(v,","),"<br/>")
+    else
+        ngx.say(k,":",v,"<br/>")
+    end
+end
+ngx.say("uri args end","<br/>")
+ngx.say("<br/>")
+
